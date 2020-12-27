7addcb36af5c747029343d8343c7038c70d2c954
ahmadsakur <57661354+b1shamonten@users.noreply.github.com>
2020-12-27 23:14:09 +0700

    coba detail post

--- www/js/index.js
+++ www/js/index.js
@@ -43,6 +43,30 @@ function onDeviceReady() {
                 html += "</div></div>";
             }
             document.getElementById("indexposts").innerHTML = html;
+            // " + post[x].idpost + "
+        }
+        return false;
+    }
+    xmlhttp.send(null);
+}
+
+function detail(idpost) {
+    xmlhttp = new XMLHttpRequest();
+    var url = "https://antarakata99.000webhostapp.com/mobile/detail.php?idpost=" + idpost;
+    var post, html = "";
+    xmlhttp.open("GET", url, true);
+    xmlhttp.onreadystatechange = function () {
+        document.getElementById("indexposts").innerHTML = 'LOADING ...';
+        if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)) {
+            post = JSON.parse(xmlhttp.responseText);
+            for (x in post) {
+                html += "<h1 class=\"post-title mt-3 p-1\">" + post[x].judul + "</h1>"
+                html += "<p class\"detail-author p-1\">Wriiten by" + post[x].nama + "</p>"
+                html += "<hr>"
+                html += "<img src=\"https://antarakata99.000webhostapp.com/asset/img/blog/" + post[x].file_gambar + "\" class=\"img-fluid rounded mb-2\" alt=\"gambar1\" style=\"width:100%\">";
+                html += "<p class\"detail-artikel p-1\">" + post[x].isi_post + "</p>"
+            }
+            document.getElementById("indexposts").innerHTML = html;
 
         }
         return false;
