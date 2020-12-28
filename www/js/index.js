4938fb48aef4371b51eb580d4765e860ce879282
dimas_andhika <dimasandhikaaa@gmail,com>
2020-12-28 19:31:54 +0700

    repair index.js

--- www/js/index.js
+++ www/js/index.js
@@ -49,27 +49,3 @@ function onDeviceReady() {
     }
     xmlhttp.send(null);
 }
-
-function detail(idpost) {
-    xmlhttp = new XMLHttpRequest();
-    var url = "https://antarakata99.000webhostapp.com/mobile/detail.php?idpost=" + idpost;
-    var post, html = "";
-    xmlhttp.open("GET", url, true);
-    xmlhttp.onreadystatechange = function () {
-        document.getElementById("indexposts").innerHTML = 'LOADING ...';
-        if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)) {
-            post = JSON.parse(xmlhttp.responseText);
-            for (x in post) {
-                html += "<h1 class=\"post-title mt-3 p-1\">" + post[x].judul + "</h1>"
-                html += "<p class=\"detail-author p-1\">" + post[x].nama + "</p>"
-                html += "<hr>"
-                html += "<img src=\"https://antarakata99.000webhostapp.com/asset/img/blog/" + post[x].file_gambar + "\" class=\"img-fluid rounded mb-2\" alt=\"gambar1\" style=\"width:100%\">";
-                html += "<p class=\"detail-artikel p-1\">" + post[x].isi_post + "</p>"
-            }
-            document.getElementById("indexposts").innerHTML = html;
-
-        }
-        return false;
-    }
-    xmlhttp.send(null);
-}
